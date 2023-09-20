0d8d65e5ad11c2591139436081f8e89056e567ad
stevenGrz13 <razafimahefasteven130102@gmail.com>
2023-09-20 16:22:00 +0200

    pushena
    pushena

--- demo5/src/main/java/com/example/demo5/solar/Connexion.java
+++ demo5/src/main/java/com/example/demo5/solar/Connexion.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class Connexion {
     public static Connection getConn() throws Exception{
-        String url="jdbc:postgresql://localhost:5432/solar";
+        String url="postgresql://postgres:4moYomLukPuibaWR7qo9@containers-us-west-204.railway.app:6885/railway";
         String user="postgres";
-        String pass="steven";
+        String pass="4moYomLukPuibaWR7qo9";
         Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection(url,user,pass);
     }
